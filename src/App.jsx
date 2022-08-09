a9a64af66170a29e2ccee60a5eb6f8b4f77b3d01
BielPimentaDev <79238035+BielPimentaDev@users.noreply.github.com>
2022-08-09 17:46:19 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import Home from './pages/Home'
+import Home from './pages/Home/Index'
 
 function App() {
  
